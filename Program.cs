d6df97041dd18337523b684c70f83623b3ccf087
Bruno Liberini <bliberini@gmail.com>
2015-10-10 15:06:19 -0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace BelatrixCodeReview
+public class JobLogger
 {
-    class Program
+    private bool _initialized;
+    private static WhereToLog _whereToLog;
+    private static LogType _logType;
+
+    public enum WhereToLog 
+    { 
+        File,
+        Console,
+        Database
+    };
+
+    public enum LogType 
+    { 
+        Message=1,
+        Error,
+        Warning
+    };
+
+    public JobLogger(WhereToLog whereToLog, LogType logType)
     {
-        static void Main(string[] args)
+        _whereToLog = whereToLog;
+        _logType = logType;
+    }
+
+    public static void LogMessage(string messageS, LogType type)
+    {
+        messageS.Trim();
+        if (messageS == null || messageS.Length == 0)
         {
+            return;
         }
+        string l = DateTime.Now.ToShortDateString() + messageS;
+        
+        if (type == LogType.Message && _logType == LogType.Message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        if (type == LogType.Error && _logType == LogType.Error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        if (type == LogType.Warning && _logType == LogType.Warning)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        try
+        {
+            System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"]);
+            connection.Open();
+            System.Data.SqlClient.SqlCommand command = new System.Data.SqlClient.SqlCommand("Insert into Log Values('" + messageS + "', " + (int)type + ")");
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            // Do something
+        }
+
+        using (System.IO.StreamWriter wr = System.IO.File.AppendText(System.Configuration.ConfigurationManager.AppSettings["LogFileDirectory"] + "LogFile" + DateTime.Now.ToShortDateString() + ".txt"))
+        {
+            wr.WriteLine(l);
+        }
+        Console.WriteLine(DateTime.Now.ToShortDateString() + messageS);
     }
 }
